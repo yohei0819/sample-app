f0652a84de5f30a1cfc8a9b69b4f9fd953fcd502
Yohei Furukawa <furukawa-yohei@rmg.co.jp>
2026-04-25 17:07:02 +0900

    fix(a11y): html lang属性をjaに修正 (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">{/* 変更: 日本語ECサイトのためja指定 */}
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
